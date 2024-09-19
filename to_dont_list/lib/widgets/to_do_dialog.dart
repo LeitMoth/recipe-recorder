f0a5533d4db44f8d825e498aa2fcf4c4516b3696
LeitMoth <col.phillips2004@gmail.com>
2024-09-18 20:38:06 -0500

    convert Todo dialog into stateless widget

--- to_dont_list/lib/widgets/to_do_dialog.dart
+++ to_dont_list/lib/widgets/to_do_dialog.dart
@@ -3,19 +3,14 @@ import 'package:flutter/material.dart';
 typedef ToDoListAddedCallback = Function(
     String value, TextEditingController textConroller);
 
-class ToDoDialog extends StatefulWidget {
-  const ToDoDialog({
+class ToDoDialog extends StatelessWidget {
+  ToDoDialog({
     super.key,
     required this.onListAdded,
   });
 
   final ToDoListAddedCallback onListAdded;
 
-  @override
-  State<ToDoDialog> createState() => _ToDoDialogState();
-}
-
-class _ToDoDialogState extends State<ToDoDialog> {
   // Dialog with text from https://www.appsdeveloperblog.com/alert-dialog-with-a-text-field-in-flutter/
   final TextEditingController _inputController = TextEditingController();
   final ButtonStyle yesStyle = ElevatedButton.styleFrom(
@@ -23,18 +18,11 @@ class _ToDoDialogState extends State<ToDoDialog> {
   final ButtonStyle noStyle = ElevatedButton.styleFrom(
       textStyle: const TextStyle(fontSize: 20), backgroundColor: Colors.red);
 
-  String valueText = "";
-
   @override
   Widget build(BuildContext context) {
     return AlertDialog(
       title: const Text('Item To Add'),
       content: TextField(
-        onChanged: (value) {
-          setState(() {
-            valueText = value;
-          });
-        },
         controller: _inputController,
         decoration: const InputDecoration(hintText: "type something here"),
       ),
@@ -48,10 +36,8 @@ class _ToDoDialogState extends State<ToDoDialog> {
               style: yesStyle,
               onPressed: value.text.isNotEmpty
                   ? () {
-                      setState(() {
-                        widget.onListAdded(valueText, _inputController);
-                        Navigator.pop(context);
-                      });
+                      onListAdded(value.text, _inputController);
+                      Navigator.pop(context);
                     }
                   : null,
               child: const Text('OK'),
@@ -64,9 +50,7 @@ class _ToDoDialogState extends State<ToDoDialog> {
           style: noStyle,
           child: const Text('Cancel'),
           onPressed: () {
-            setState(() {
-              Navigator.pop(context);
-            });
+            Navigator.pop(context);
           },
         ),
       ],
